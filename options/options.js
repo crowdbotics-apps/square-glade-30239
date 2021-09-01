3453f21ee01f098ec08b1946cec4b7047640a1e0
Crowdbotics <team@crowdbotics.com>
2021-09-01 05:01:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_glade_30239Identifier",
+  url: "https://square_glade_30239Identifier.botics.co",
+  api: "https://square_glade_30239Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
